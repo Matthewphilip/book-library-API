ebe10ef91f98470af01a6f318b1fe8bada9acc59
Matthew Wiggins <mattwiggins5@gmail.com>
2022-06-16 17:32:36 +0100

    addition of CRUD - issue

--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -1,22 +1,58 @@
 const { Reader } = require('../models');
 
 exports.create = async (req, res) => {
+
+    try {
     const newReader = await Reader.create(req.body);
     res.status(201).json(newReader);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
 exports.readAll = async (req, res) => {
-    const readers = await Reader.findAll({where: req.query});
-        res.status(201).json(readers)
+
+    try {
+        const readers = await Reader.findAll({where: req.query});
+        res.status(200).json(readers);  
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+
+exports.readById = async (req, res) => {
+    const { readerId } = req.params;
+    const reader = await Reader.findByPk(readerId);
+
+    try {
+        res.status(200).json(reader);
+    } catch (err) {
+        res.status(404).json(err);
+    }
+};
+
+exports.update = async (req, res) => {
+    const { readerId } = req.params;
+    const updateData = req.body;
+    const [ updatedRows ] = await Reader.update(updateData, {where:{ id: readerId } });
+    if(!updatedRows) {
+      res.status(404).json({ error: "The reader does not exist" });
+    } else {
+      const updatedItem = await Reader.findByPk(readerId);
+      res.status(200).json(updatedItem);
+    }
+};
+
+exports.destroy = async (req, res) => {
+    const { readerId } = req.params;
+    const deleteData = await Reader.destroy({ where: { id: readerId } });
+    if(deleteData) {
+        res.status(204).send();
+    } else {
+        res.status(404).send({ error: "The reader could not be found" })
     }
 
-// exports.readById = (req, res) => {
-//     const {readerId} = req.params;
-//     const reader = await Reader.findByPk(readerId);
+};
 
-//     if(!reader) {
-//         res.status(404).json({error: "The reader could not be found"});
-//     } else {
-//         res.status(201).json(reader)
-//     }
-// }
+  
--- src/routes/reader.js
+++ src/routes/reader.js
@@ -4,6 +4,9 @@ const readerController = require('../controllers/reader');
 const router = express.Router();
 
 router.post('/', readerController.create);
-router.get('/', readerController.read);
+router.get('/', readerController.readAll);
+router.get('/:id', readerController.readById);
+router.patch('/:readerId', readerController.update);
+router.delete('/:readerId', readerController.destroy);
 
 module.exports = router;
