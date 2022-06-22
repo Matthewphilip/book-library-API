8ce211d939c0d0ad496c64ae7d03f94495ba1561
Matthew Wiggins <mattwiggins5@gmail.com>
2022-06-22 12:03:08 +0100

    author added

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const readerRouter = require('./routes/reader');
 const booksRouter = require('./routes/books');
+const authorRouter = require('./routes/author');
 
 app.use(express.json());
 app.use('/readers', readerRouter);
 app.use('/books', booksRouter);
+app.use('/authors', authorRouter);
 
 app.get('/', (req, res) => {
     res.status(200).json({ result: "Hello World" });
