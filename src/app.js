f2621d4a3100d44730b4ffcbb12eafe0cbd43fed
Matthew Wiggins <mattwiggins5@gmail.com>
2022-06-25 11:43:30 +0100

    genre added

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const app = express();
 const readerRouter = require('./routes/reader');
 const booksRouter = require('./routes/books');
 const authorRouter = require('./routes/author');
+const genreRouter = require('./routes/genre');
 
 app.use(express.json());
 app.use('/readers', readerRouter);
 app.use('/books', booksRouter);
 app.use('/authors', authorRouter);
+app.use('/genres', genreRouter);
 
 app.get('/', (req, res) => {
     res.status(200).json({ result: "Hello World" });
