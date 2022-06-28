1275b0d6d7ecc42a6f32ce8651c0c8d3674c5e68
Matthew Wiggins <mattwiggins5@gmail.com>
2022-06-28 13:26:07 +0100

    associations added

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 const ReaderModel = require('./reader');
 const BookModel = require('./book');
-const AuthorModel = require('./author');
 const GenreModel = require('./genre');
+const AuthorModel = require('./author');
 
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
@@ -16,15 +16,20 @@ const setupDatabase = () => {
 
   const Reader = ReaderModel(connection, Sequelize);
   const Book = BookModel(connection, Sequelize);
-  const Author = AuthorModel(connection, Sequelize);
   const Genre = GenreModel(connection, Sequelize);
+
+  Reader.hasMany(Book);
+  Genre.hasMany(Book);
+  Book.belongsTo(Genre);
+
+  const Author = AuthorModel(connection, Sequelize);
 
   connection.sync({ alter: true });
   return {
     Reader,
     Book,
-    Author,
     Genre,
+    Author,
   };
 };
 
